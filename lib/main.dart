5e1ac67c1d06ac24f9e58961de1d071cf3a65bb4
whisnuys <whisnusaputra30@gmail.com>
2023-04-08 22:29:45 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => TasksBloc(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Tasks App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
